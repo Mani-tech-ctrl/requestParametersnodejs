8d843c7ec32fb9e18d47760de23c0960cf8958ed
@Mani_tech <123249175+Mani-tech-ctrl@users.noreply.github.com>
2025-11-11 15:55:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app =   express();
+
+let port = 8080;
+
+app.listen(port, ()=>{
+    console.log(`app is listening on port ${port}`);
+});
+
+
+
+app.get("/", (req, res) => {
+    console.log(req.params);
+    res.send("Hello, i am root path");
+});
+
+app.get('/:username/:id', (req, res) =>{
+    let {username, id } = req.params;
+    res.send(`welcome@${username}${id} Update your task`);
+
+});
+
+
+
+
+
+
+//listen all request
+// app.use((req, res)=>{
+//     console.log("request received");
+//     let code = "<h1>Student's Name <h1> <ul>Asutosh</ul> <ul>Abhishek</ul> <ul>Poonam</ul> <ul>Kavita</ul> <ul>Rahul</ul>";
+//     res.send(code);
+// });
+
